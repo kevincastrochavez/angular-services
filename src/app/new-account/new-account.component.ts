6fd327b7f2c8560ba95a1b74adc5782c9b0f4133
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-17 18:34:20 -0600

    learned how to inject services into services

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -7,16 +7,14 @@ import { LoggingService } from "../logging.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
-    private loggingService: LoggingService,
+    // private loggingService: LoggingService,
     private accountsService: AccountService
   ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
